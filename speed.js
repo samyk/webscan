fe7d9d77241b60f0e8aa48439ecc8bad8a3f8816
Samy Kamkar <git@samy.pl>
2020-11-08 13:18:27 -0800

    speed test

--- speed.js
+++ speed.js
@@ -1,30 +1,46 @@
 // test speeds of different methods of scanning hosts
-
 let speeds = {1: 0, 2: 0}
-let tests = 1000
-speed()
+let tests = 1000000
+
+let funcs = {
+  gettime() { new Date().getTime() },
+  perf() { performance.now() },
+}
+
 async function speed()
 {
+  for (let [name, fn] of Object.entries(funcs))
+  {
+    console.log(`TEST: ${name}`)
+    let t1 = performance.now()
+    for (let i = 0; i < tests; i++)
+      await fn()
+    let t2 = performance.now()
+    console.log(`TEST: ${name}: ${(t2-t1)} / ${tests} = ${(t2-t1)/tests}`)
+  }
+  /*
   let id = 1
-  console.log('t1, sig')
+  console.log('f1, sig')
   for (let i = 0; i < tests; i++)
-    await t1()
+    await f1()
   console.log(id+': '+speeds[id]+'/'+tests+'='+(speeds[id]/tests))
 
   id++
-  console.log('t2, big fetch')
+  console.log('f2, big fetch')
   for (let i = 0; i < tests; i++)
-    await t2()
+    await f2()
   console.log(id+': '+speeds[id]+'/'+tests+'='+(speeds[id]/tests))
+  */
 }
+speed()
 
-async function t1()
+async function f1()
 {
   const controller = new AbortController()
   const { signal } = controller
   await t({ signal: signal }, controller, 1)
 }
-async function t2()
+async function f2()
 {
   const controller = new AbortController()
   const { signal } = controller
