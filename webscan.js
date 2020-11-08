0673df22d0179ad49bc8ac7a9cbc0d257564f6e9
Samy Kamkar <git@samy.pl>
2020-11-07 16:04:26 -0800

    add webrtc handling

--- webscan.js
+++ webscan.js
@@ -1,5 +1,5 @@
 /*
- * web-based network scanner
+ * reliable web-based network scanner
  * by samy kamkar 2020/11/07
  * https://samy.pl
  */
@@ -43,8 +43,224 @@ let subnets = [
   //'200.200.200.5',
 ]
 
+function dump(obj, indent)
+{
+  let result = ""
+  if (indent == null) indent = ""
+
+  for (let property in obj)
+  {
+    let value = obj[property]
+    if (typeof value == 'string')
+      value = "'" + value + "'"
+    else if (typeof value == 'object')
+    {
+      if (value instanceof Array)
+      {
+        // Just let JS convert the Array to a string!
+        value = "[ " + value + " ]"
+      }
+      else
+      {
+        // Recursive dump
+        // (replace "  " by "\t" or something else if you prefer)
+        let od = dump(value, indent + "  ")
+        // If you like { on the same line as the key
+        //value = "{\n" + od + "\n" + indent + "}"
+        // If you prefer { and } to be aligned
+        value = "\n" + indent + "{\n" + od + "\n" + indent + "}"
+      }
+    }
+    result += indent + "'" + property + "' : " + value + ",\n"
+  }
+  return result.replace(/,\n$/, "")
+}
+
+// ascii to hex
+function a2h(str)
+{
+  let hex = []
+  for (let n = 0; n < str.length; n++)
+  {
+    let hbyte = Number(str.charCodeAt(n)).toString(16)
+    if (hbyte.length == 1)
+      hbyte = "0" + hbyte
+    hex.push(hbyte)
+  }
+  return hex.join('')
+}
+
+// Connect the two peers. Normally you look for and connect to a remote
+// machine here, but we're just connecting two local objects, so we can
+// bypass that step.
+window.connectPeers = async function(ip, success)
+{
+  let localConnection = null // RTCPeerConnection for our "local" connection
+  let remoteConnection = null  // RTCPeerConnection for the "remote"
+
+  let sendChannel = null // RTCDataChannel for the local (sender)
+  let receiveChannel = null    // RTCDataChannel for the remote (receiver)
+
+  // Handles clicks on the "Send" button by transmitting
+  // a message to the remote peer.
+  function sendMessage()
+  {
+    sendChannel.send('test')
+  }
+
+  // Handle status changes on the local end of the data
+  // channel this is the end doing the sending of data
+  // in this example.
+  function handleSendChannelStatusChange(event)
+  {
+    console.log('handleSendChannelStatusChange', sendChannel)
+    if (sendChannel)
+    {
+      console.log('sendChannel state: ' + sendChannel.readyState)
+      if (sendChannel.readyState === 'open')
+        sendMessage()
+    }
+  }
+
+  // Called when the connection opens and the data
+  // channel is ready to be connected to the remote.
+  function receiveChannelCallback(event)
+  {
+    console.log(`receiveChannelCallback: ${event}`, event)
+    //document.getElementById('content').innerHTML += '<b>XXXreceiveChannelCallback:</b>'+dump(event, '  ')
+    receiveChannel = event.channel
+    receiveChannel.onmessage = handleReceiveMessage
+    receiveChannel.onopen = handleReceiveChannelStatusChange
+    receiveChannel.onclose = handleReceiveChannelStatusChange
+  }
+
+
+  // Handle status changes on the receiver's channel.
+  function handleReceiveChannelStatusChange(event)
+  {
+    console.log(`handleReceiveChannelStatusChange`)
+    if (receiveChannel)
+      console.log("Receive channel's status has changed to " + receiveChannel.readyState)
+  }
+
+  // Close the connection, including data channels if they're open
+  // Also update the UI to reflect the disconnected status
+  function disconnectPeers()
+  {
+    console.log(`disconnectPeers`)
+    // Close the RTCDataChannels if they're open
+    sendChannel.close()
+    receiveChannel.close()
+
+    // Close the RTCPeerConnections
+    localConnection.close()
+    remoteConnection.close()
+
+    sendChannel = null
+    receiveChannel = null
+    localConnection = null
+    remoteConnection = null
+  }
+
+  // Create the local connection and its event listeners
+  const config = {
+    iceServers: [],
+    iceTransportPolicy: 'all',
+    iceCandidatePoolSize: 0,
+  }
+  localConnection = new RTCPeerConnection(config)
+
+  // Create the data channel and establish its event listeners
+  sendChannel = localConnection.createDataChannel("sendChannel")
+  sendChannel.onopen = async function(e) { success(ip) }
+  //sendChannel.onopen = handleSendChannelStatusChange
+  sendChannel.onclose = handleSendChannelStatusChange
+
+  // Create the remote connection and its event listeners
+  remoteConnection = new RTCPeerConnection()
+  remoteConnection.ondatachannel = receiveChannelCallback
+
+  // Set up the ICE candidates for the two peers
+  localConnection.onicecandidate = function(e)
+  {
+    if (e.candidate)
+    {
+      let newcan = {}
+      for (let key of ["address", "candidate", "component", "foundation", "port", "priority", "protocol", "relatedAddress", "relatedPort", "sdpMLineIndex", "sdpMid", "tcpType", "type", "usernameFragment"])
+        newcan[key] = e.candidate[key]
+      newcan.candidate = newcan.candidate.replaceAll(/[\w\-]+\.local|127\.0\.0\.1/g, ip)
+      newcan.address = ip
+      return localConnection.addIceCandidate(newcan)
+    }
+    return !e.candidate || remoteConnection.addIceCandidate(e.candidate)
+  }
+
+  remoteConnection.onicecandidate = function(e)
+  {
+    if (e.candidate)
+    {
+      let newcan = {}
+      for (let key of ["address", "candidate", "component", "foundation", "port", "priority", "protocol", "relatedAddress", "relatedPort", "sdpMLineIndex", "sdpMid", "tcpType", "type", "usernameFragment"])
+        newcan[key] = e.candidate[key]
+      newcan.candidate = newcan.candidate.replaceAll(/[\w\-]+\.local|127\.0\.0\.1/g, ip)
+      newcan.address = ip
+      return localConnection.addIceCandidate(newcan)
+    }
+    return !e.candidate || localConnection.addIceCandidate(e.candidate)
+  }
+
+  // Now create an offer to connect this starts the process
+  localConnection.createOffer()
+  .then(offer => localConnection.setLocalDescription(offer))
+  .then(() => remoteConnection.setRemoteDescription(localConnection.localDescription))
+  .then(() => remoteConnection.createAnswer())
+  .then(answer => remoteConnection.setLocalDescription(answer))
+  .then(() => localConnection.setRemoteDescription(remoteConnection.localDescription))
+  .catch(handleCreateDescriptionError)
+}
+
+// Handle errors attempting to create a description
+// this can happen both when creating an offer and when
+// creating an answer. In this simple example, we handle
+// both the same way.
+function handleCreateDescriptionError(error)
+{
+  console.log("Unable to create an offer: " + error.toString())
+}
+
+// Handle successful addition of the ICE candidate
+// on the "local" end of the connection.
+function handleLocalAddCandidateSuccess()
+{
+  console.log('handleLocalAddCandidateSuccess')
+}
+
+// Handle successful addition of the ICE candidate
+// on the "remote" end of the connection.
+function handleRemoteAddCandidateSuccess()
+{
+  console.log('handleRemoteAddCandidateSuccess')
+}
+
+// Handle an error that occurs during addition of ICE candidate.
+
+function handleAddCandidateError()
+{
+  console.log(`handleAddCandidateError - FAIL`)
+}
+
+// Handle onmessage events for the receiving channel.
+// These are the data messages sent by the sending channel.
+
+function handleReceiveMessage(event)
+{
+  console.log(`handleReceiveMessage: ${event}: ${event.data}`)
+  //document.getElementById('content').innerHTML += '\n<b>XXXhandleReceiveMessage:</b>'+dump(event, '  ')
+  console.log(event.ice)
+}
+
 // convert * in ips to 0..255
-var unroll_ips = function(ips, min, max)
+let unroll_ips = function(ips, min, max)
 {
   // convert single ip to array
   if (typeof(ips) === 'string')
@@ -53,7 +269,7 @@ var unroll_ips = function(ips, min, max)
   // flatten *
   return ips.flatMap(ip =>
     ip.indexOf('*') != -1 ?
-    [...Array(256-(min||0)-(max||0)).keys()].map(i => ip.replace('*', (min || 0) + i)) :
+    [...Array((max-(min||0)||256)-(min||0)).keys()].map(i => ip.replace('*', (min || 0) + i)) :
     ip
   )
 }
@@ -80,7 +296,7 @@ window.scanIpsBlock = async function(ips, conf)
     window.location.protocol = 'http:'
 
   if (!conf) conf = { }
-  if (!conf.timeout) conf.timeout = 1000
+  if (!conf.timeout) conf.timeout = 1500
   if (conf.logger) conf.logger(`scanIpsBlock(${ips})`)
   let promises = {}
   let liveIps = {}
@@ -89,10 +305,21 @@ window.scanIpsBlock = async function(ips, conf)
   const { signal } = controller
 
   // add ip to our live IPs
-  let addLive = function(ip, time) { liveIps[ip] = time }
+  let addLive = async function(ip, time)
+  {
+    liveIps[ip] = time
+
+    // now validate which ips are actually local via webrtc
+    if (conf.rtc !== false)
+      await connectPeers(ip, function(ip)
+      {
+        if (conf.logger) conf.logger(`<b>found LOCAL ip address: ${ip}</b>`)
+        liveIps[ip] = 0
+      })
+  }
 
   // generate success/fail promises first to speed things up
-  for (var ip of ips)
+  for (let ip of ips)
     promises[ip] = (function(ip) {
       return function(e)
       {
@@ -109,17 +336,21 @@ window.scanIpsBlock = async function(ips, conf)
   }, conf.timeout)
 
   // scan our ips
-  let start = new Date().getTime()
-  for (var ip of ips)
-    scans.push(fetch(`http://${ip}:1337/samyscan`, { signal }).then(promises[ip]).catch(promises[ip]))
+  let start = {}
+  for (let ip of ips)
+  {
+    scans.push(fetch(`http://${ip}:1337/samyscan`, { signal }).catch(promises[ip]))
+    start[ip] = new Date().getTime()
+  }
+      //scans.push(fetch(`http://${ip}:1337/samyscan`, { signal }).then(promises[ip]).catch(promises[ip]))
 
   // when everything's done scanning, get time in ms
   await Promise.all(scans.map(p => p.catch(e => e))).then(v => {
-    if (conf.logger) conf.logger('scanIpsBlock promises done')
-    for (var [ip, end] of Object.entries(liveIps))
-      liveIps[ip] -= start
+    for (let [ip, end] of Object.entries(liveIps))
+      if (liveIps[ip])
+        liveIps[ip] -= start[ip]
   })
-  if (conf.logger) conf.logger('scanIpsBlock done')
+  //if (conf.logger) conf.logger('scanIpsBlock done')
 
   // return ip: time
   return liveIps
@@ -134,7 +365,8 @@ function getSubnet(ip)
 // scan for subnets, then scan discovered subnets for IPs
 window.webScanAll = async function(nets, conf)
 {
-  let possible = []
+  let possible = {}
+  let ips = {}
   if (!conf) conf = { }
   if (!nets) nets = subnets
   if (conf.logger) conf.logger(`webScanAll() started`)
@@ -143,12 +375,16 @@ window.webScanAll = async function(nets, conf)
   nets = await scanIps(unroll_ips(nets), conf)
 
   // now let's scan each block of 256 IPs per net
-  for (var net of Object.keys(nets))
+  for (let net of Object.keys(nets))
   {
     if (conf.logger) conf.logger(`scanIps(${getSubnet(net)})`)
-    possible.push(await scanIps(unroll_ips(getSubnet(net)+'*', 2, 253), conf))
+    Object.assign(possible, await scanIps(unroll_ips(getSubnet(net)+'*', 2, 253), conf))
   }
 
-  return possible
+  ips.network = possible
+  ips.local = Object.keys(possible).filter(ip => possible[ip] === 0)
+  return ips
 }
 })(window)
+
+
